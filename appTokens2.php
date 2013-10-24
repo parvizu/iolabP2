ab94a2be6c3642be1a0769e33cdfa629023b09d0
Pablo <parvizu@ischool.berkeley.edu>
2013-10-24 09:28:50 -0700

    Added missing AUTH files for the server side

---
+++ appTokens2.php
@@ -0,0 +1,13 @@
+<?php 
+
+$consumer_key = 'IYQOETLsZzGuUPjJ2PunDA'; 
+
+$consumer_secret = 'K06mCXsridXC7I8B6sgfYpxyYCiDXWouzCifaNp6io'; 
+
+$user_token = '1961166864-fvOKPLuzWC0mHzL0q9LhryxlaMBjtKsVGOaAx9N'; 
+
+$user_secret = 'SXODYoEUxlYDp7J6HYXzuYjNnYXGG10YKrF84GmqQj4'; 
+
+ 
+
+?> 
